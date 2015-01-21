8805d34f4a395aff2bb0973a683ee3eeada7fec5
Norman-by <norman_by@tut.by>
2015-01-21 08:46:22 +0300

    1.0.6
    оптимизирован код

--- konverterForOnliner.js
+++ konverterForOnliner.js
@@ -2,10 +2,12 @@
 // @name        Konverter
 // @namespace   konverter.onliner.by
 // @include     http://*.onliner.by*
-// @version     1.0.5
+// @version     1.0.6
 // @author      Norman_by
+// @source      https://github.com/Norman-by/konverter
 // @grant       none
 // ==/UserScript==
+
 var kurs = $('.top-informer-currency>a>span._u').text();
 var expr = /\s/gi; 
 var chislo = parseInt( kurs.replace(expr,'').slice(1), 10);
@@ -13,59 +15,54 @@ var chislo = parseInt( kurs.replace(expr,'').slice(1), 10);
  
 
 function convertKurs(selector) {
-   selector.map(function(indx, element){
-     
-     var summByr = parseInt(($(element).text().replace(expr,'')), 10);
-     var summDol = '$ ' + String(Math.round(summByr/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
-   
-      $(element).text(summDol).attr({"title":summByr});
-     $(element).parent().html( $(element).parent().html().replace("руб.",''));
-  
-     
+	selector.map(function(indx, element){
+		var summByr = parseInt(($(element).text().replace(expr,'')), 10);
+		var summDol = '$ ' + String(Math.round(summByr/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+	   
+		$(element).text(summDol).attr({"title":summByr});
+		$(element).parent().html( $(element).parent().html().replace("руб.",'')); 
   });
 };
+
 function convertKursKatalog(selector) {
    selector.map(function(indx, element){
-    
-    var arrTmp = $(element).html().split('&nbsp;руб.');
-     arrTmp[0]= arrTmp[0].replace(expr,'').split("-");
+		var arrTmp = $(element).html().split('&nbsp;руб.');
+		arrTmp[0]= arrTmp[0].replace(expr,'').split("-");
       
-    if (arrTmp[0].length == 2) {
+		if (arrTmp[0].length == 2) 
+		{
 			var summByr = parseInt(( arrTmp[0][0] ), 10);  
 			var summByr2 = parseInt(( arrTmp[0][1] ), 10);  
- if ($.isNumeric(summByr)) 
-    var summDol = '$ ' + String(Math.round(summByr/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' - ' + '$ ' + String(Math.round(summByr2/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
-
-			 
-        } else {
-      var summByr = parseInt(( arrTmp[0][0] ), 10);  
-  if ($.isNumeric(summByr)) 
-     var summDol = '$ ' + String(Math.round(summByr/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+			if ($.isNumeric(summByr)) 
+				var summDol = '$ ' + String(Math.round(summByr/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' - ' + '$ ' + String(Math.round(summByr2/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+		} else 
+		{
+			var summByr = parseInt(( arrTmp[0][0] ), 10);  
+			if ($.isNumeric(summByr)) 
+				var summDol = '$ ' + String(Math.round(summByr/chislo)).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
         }
-	 
-     if(!!!summDol) {
-    		if (!!arrTmp[1]) $(element).html(summDol+arrTmp[1]);
-  	} 
+	   
+		if(!!summDol&&!!arrTmp[1])  
+			$(element).html(summDol+arrTmp[1]);
+		if(!!summDol&&!!!arrTmp[1])  
+			$(element).html(summDol);
      });
 };
 
 /*-------*/
-setTimeout(function () {
-      $(document).ajaxSuccess(function(){
-         
-        convertKurs($('p.big>a>strong')); //авто-мото барахолка
+function krrasota() {
+		convertKurs($('p.big>a>strong')); //авто-мото барахолка
         convertKurs($('td.cost>big>strong')); //барахолка
         convertKursKatalog($('td.pprice')); //каталог
         convertKurs($('ul.b-ba-topicdet>li.cost')); //барахолка объявление
 		convertKurs($('span.autoba-hd-details-costs>span.cost')); //авто-барахолка объявление
+  
+}
+setTimeout(function () {
+      $(document).ajaxSuccess(function(){
+         krrasota();
      });
       
-       
-		convertKurs($('p.big>a>strong')); //авто-мото барахолка
-		convertKurs($('td.cost>big>strong')); //барахолка
-		convertKursKatalog($('td.pprice')); //каталог
-		convertKurs($('ul.b-ba-topicdet>li.cost')); //барахолка объявление
-		convertKurs($('span.autoba-hd-details-costs>span.cost>strong')); //авто-барахолка объявление
-   
-    
+     krrasota();
 }, 2000);
+
